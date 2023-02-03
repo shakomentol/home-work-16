4b470cd38ea7cc8df96230f8c8c8da8f93eb09dc
shako metonidze <120588118+shakomentol@users.noreply.github.com>
2023-02-03 12:25:13 -0800

    add some js and css styles

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 const button = document.querySelector(".button-icon");
 button.addEventListener("click",() => {
     button.remove();
-    const src = document.getElementById("body");
-    src.appendChild(img);
+    // const src = document.getElementById("body");
+    // src.appendChild(img);
 })
 // 2.
 const img = document.createElement("img");
 img.setAttribute("src", "https://media.tenor.com/LRTkUYkU1kkAAAAC/ok-okay.gif");
 img.setAttribute("alt", "okay");
-// const src = document.getElementById("body");
-// src.appendChild(img);
+img.style.marginLeft = "10%";
+img.style.marginTop = "50px";
+img.style.borderRadius = "10px"
+const src = document.getElementById("body");
+src.appendChild(img);
