92673f017617bf1856ae743d76d695fea1166ad8
shako metonidze <120588118+shakomentol@users.noreply.github.com>
2023-02-04 06:37:38 -0800

    add js styles

--- script.js
+++ script.js
@@ -14,3 +14,7 @@ img.style.marginTop = "50px";
 img.style.borderRadius = "10px"
 const src = document.getElementById("body");
 src.appendChild(img);
+// 3.
+const productData = [{product_id: 1, amount: 200}, { product_id: 2, amount: 300 }, { product_id:3, amount: 500 }]
+const sum = (productData) => productData + productData;
+console.log(productData);
